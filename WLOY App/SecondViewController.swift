9d4ea66e9bbc580d002ccf287f2eedce52cccffd
William Robbins <wrobbins@minnie.cs.loyola.edu>
2016-03-15 16:23:59 -0400

    Initial Commit

---
+++ WLOY App/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WLOY App
+//
+//  Created by William Robbins on 3/15/16.
+//  Copyright © 2016 William Robbins. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
