a805b880f07bae1b91b51502fe68ea0fb2ab3ce5
William Robbins <wrobbins@minnie.cs.loyola.edu>
2016-03-31 12:16:12 -0400

    Added support for XML parsing song data

--- WLOY App/NowPlayingViewController.swift
+++ WLOY App/NowPlayingViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class NowPlayingViewController: UIViewController {
 
@@ -14,15 +15,33 @@ class NowPlayingViewController: UIViewController {
     let DEFAULT_ARTIST = "Artist Name"
     let DEFAULT_SHOW = "Show Name"
     let DEFAULT_SONG = "Song Name"
+    let FEED_ADDRESS = "http://wloy.radioactivity.fm/feeds/last10.xml"
+    let UPDATE_INTERVAL = 5 // duration of XML update delay, in seconds
+    let STREAM_ADDRESS = "http://war.str3am.com:8130/live"
+    
+    // DATA MEMBERS
+    var audioPlayer: AVPlayer!
+    var xmlParser: NSXMLParser!
+    var xmlParserDelegate: WLOYSongParserDelegate!
     
     // OUTLETS
+    @IBOutlet weak var showNameLabel: UILabel!
     @IBOutlet weak var volume: UISlider!
     @IBOutlet weak var nowPlayingLabel: UILabel!
     
     // METHODS
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadAudioPlayer()
         setNowPlayingLabel(DEFAULT_ARTIST, song:DEFAULT_SONG)
+        setCurrentShowLabel(DEFAULT_SHOW)
+        
+        // Initialize XML parser
+        xmlParser = NSXMLParser.init(contentsOfURL:NSURL.init(string: FEED_ADDRESS)!)
+        xmlParserDelegate = WLOYSongParserDelegate.init()
+        xmlParser.delegate = xmlParserDelegate
+        checkXML()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,12 +50,33 @@ class NowPlayingViewController: UIViewController {
     
     // setCurrentShowLabel - update text on the current show field
     func setCurrentShowLabel(show: String) {
-        
+        showNameLabel.text = show
     }
     
     // setNowPlayingLabel - given an artist and song, update the now playing field
     func setNowPlayingLabel(artist: String, song: String) {
         nowPlayingLabel.text = artist + " - " + song
+    }
+    
+    // startTimerThread - create a thread that starts listening for XML updates
+    func startTimerThread() {
+        // TODO: Actually get this to work
+        NSLog("Timer thread started! (not really)")
+        checkXML()
+    }
+    
+    // checkXML - checks XML file and updates fields to appropriate values
+    func checkXML() {
+        xmlParser.parse()
+        setNowPlayingLabel(xmlParserDelegate.currentArtist, song:xmlParserDelegate.currentSong)
+    }
+    
+    // loadAudioPlayer - (re)loads the audio stream into audioPlayer
+    func loadAudioPlayer() {
+        audioPlayer = AVPlayer.init(URL:NSURL.init(string:STREAM_ADDRESS)!)
+        audioPlayer.rate = 1.0
+        audioPlayer.volume = volume.value // TODO: Adjust volume as slider changes
+        audioPlayer.play()
     }
 
 }
---
+++ WLOY App/WLOYSongParserDelegate.swift
@@ -0,0 +1,97 @@
+//
+//  WLOYSongParserDelegate.swift
+//  WLOY App
+//
+//  Created by William Robbins on 3/31/16.
+//  Copyright © 2016 William Robbins. All rights reserved.
+//
+
+import Foundation
+
+class WLOYSongParserDelegate: NSObject, NSXMLParserDelegate {
+    
+    // CONSTANTS
+    let DEFAULT_SONG = "SONG NOT FOUND"
+    let DEFAULT_ARTIST = "ARTIST NOT FOUND"
+    let XML_ITEM = "item"
+    let XML_SONG_TITLE = "ra:track"
+    let XML_SONG_ARTIST = "ra:artist"
+    
+    // DATA MEMBERS
+    var currentSong: String
+    var currentArtist: String
+    var firstSongFound: Bool
+    var parsingFirstSong: Bool
+    var parsingTitle: Bool
+    var parsingArtist: Bool
+    var tempCurrentSong: String
+    var tempCurrentArtist: String
+    
+    override init() {
+        currentSong = DEFAULT_SONG
+        currentArtist = DEFAULT_ARTIST
+        tempCurrentSong = ""
+        tempCurrentArtist = ""
+        firstSongFound = false
+        parsingFirstSong = false
+        parsingTitle = false
+        parsingArtist = false
+    }
+    
+    // parserDidStartDocument - called when the parser begins reading the XML document
+    func parserDidStartDocument(parser:NSXMLParser) {
+        firstSongFound = false
+        parsingFirstSong = false
+        tempCurrentSong = ""
+        tempCurrentArtist = ""
+    }
+    
+    // didStartElement - called when the parser begins parsing an XML element
+    func parser(_ parser:NSXMLParser, didStartElement elementName:String, namespaceURI namespaceURI:String?, qualifiedName qName:String?, attributes attributeDict:[String : String]) {
+            // Only parse this if the first song has not been found
+            if(!firstSongFound) {
+                if(elementName == XML_ITEM) {
+                    parsingFirstSong = true
+                }
+                else if(parsingFirstSong) {
+                    if(elementName == XML_SONG_TITLE) {
+                        parsingTitle = true
+                    }
+                    else if(elementName == XML_SONG_ARTIST) {
+                        parsingArtist = true
+                    }
+                }
+            }
+    }
+    
+    // didEndElement - called when the parser finishes parsing an XML element
+    func parser(_ parser:NSXMLParser, didEndElement elementName:String, namespaceURI namespaceURI:String?, qualifiedName qName:String?) {
+        if(parsingFirstSong) {
+            if(elementName == XML_ITEM) {
+                parsingFirstSong = false
+                firstSongFound = true
+                currentSong = tempCurrentSong
+                currentArtist = tempCurrentArtist
+            }
+            else if(elementName == XML_SONG_TITLE) {
+                parsingTitle = false
+            }
+            else if(elementName == XML_SONG_ARTIST) {
+                parsingArtist = false
+            }
+        }
+    }
+    
+    // foundCharacters - called when the parser retrieves new characters within an element
+    func parser(_ parser:NSXMLParser, foundCharacters string:String) {
+        if(parsingFirstSong) {
+            if(parsingTitle) {
+                tempCurrentSong = tempCurrentSong + string
+            }
+            else if(parsingArtist) {
+                tempCurrentArtist = tempCurrentArtist + string
+            }
+        }
+    }
+    
+}
