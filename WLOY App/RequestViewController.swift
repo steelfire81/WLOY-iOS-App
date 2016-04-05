99d9f9d7585875e7a652ea6b73716f8a67d74eda
Billy Robbins <wcrobbins@loyola.edu>
2016-04-05 18:13:46 -0400

    Implemented request fields to request menu

--- WLOY App/RequestViewController.swift
+++ WLOY App/RequestViewController.swift
@@ -10,12 +10,39 @@ import UIKit
 
 class RequestViewController: UIViewController {
 
+    // CONSTANTS - Error Messages
+    let ERR_NOT_ENOUGH_INFO = "Please provide a song title and artist"
+    
+    // OUTLETS
+    @IBOutlet weak var songTitleField: UITextField!
+    @IBOutlet weak var artistField: UITextField!
+    
+    // METHODS
     override func viewDidLoad() {
         super.viewDidLoad()
+        songTitleField.text = ""
+        artistField.text = ""
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
+    @IBAction func requestSong(sender: AnyObject) {
+        let title = songTitleField.text
+        let artist = artistField.text
+        
+        // Ensure both fields are entered
+        if((title == "") || (artist == "")) {
+            NSLog(ERR_NOT_ENOUGH_INFO) // TODO: Make a pop-up notification
+            return
+        }
+        
+        // All info provided; send request
+        NSLog("Requesting " + title! + " by " + artist!) // TODO: Actually send request to server
+        
+        // Clear fields
+        songTitleField.text = ""
+        artistField.text = ""
+    }
 }
