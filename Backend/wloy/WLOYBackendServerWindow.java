17ae81b6949e715af45dd3fdf8aeebbf7dbae35b
Billy Robbins <wcrobbins@loyola.edu>
2016-05-03 14:19:46 -0400

    Removed unnecessary imports
     - removed an unnecessary import from WLOYBackendServerWindow.java

--- Backend/wloy/WLOYBackendServerWindow.java
+++ Backend/wloy/WLOYBackendServerWindow.java
@@ -2,10 +2,8 @@ package wloy;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
